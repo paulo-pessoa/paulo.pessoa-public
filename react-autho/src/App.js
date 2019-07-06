33d56c0af9a7a279bf792a4b72892c658ac920c8
paulo-pessoa <paulo.pessoa@daugherty.com>
2019-07-06 10:25:01 -0500

    - finished chapter 6 of react-auth0

--- react-autho/src/App.js
+++ react-autho/src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
+import Auth from "./auth/Auth";
+import Callback from "./Callback";
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.auth = new Auth(this.props.history);
+    }
+
   render() {
     return (
       <>
-        <Nav />
+        <Nav auth={this.auth}/>
         <div className="body">
-          <Route path="/" exact component={Home} />
-          <Route path="/profile" component={Profile} />
+          <Route path="/" exact render = { props => <Home auth={this.auth} {...props}  /> } />
+            <Route
+                path="/callback"
+                render={props => <Callback auth={this.auth} {...props} />}
+            />
+            <Route
+                path="/profile"
+                render={props =>
+                    this.auth.isAuthenticated() ? (
+                        <Profile auth={this.auth} {...props} />
+                    ) : (
+                        <Redirect to="/" />
+                    )
+                }
+            />
         </div>
       </>
     );
